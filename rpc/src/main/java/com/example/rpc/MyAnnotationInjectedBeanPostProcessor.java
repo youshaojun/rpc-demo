70a28e20d9bc52e8bcea7c28de1bc7b0e643bc4b
dianjishamaoxian <15639720785@163.com>
2021-04-02 18:34:48 +0800

    重写postProcessProperties方法实现属性注入逻辑

---
+++ rpc/src/main/java/com/example/rpc/MyAnnotationInjectedBeanPostProcessor.java
@@ -0,0 +1,27 @@
+package com.example.rpc;
+
+import org.springframework.beans.PropertyValues;
+import org.springframework.beans.factory.annotation.AutowiredAnnotationBeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+
+/**
+ * Created by admin on 2021/3/31 9:44
+ * @author admin
+ */
+@Component
+public class MyAnnotationInjectedBeanPostProcessor extends AutowiredAnnotationBeanPostProcessor {
+
+//    @Override
+//    public PropertyValues postProcessProperties(PropertyValues pvs, Object bean, String beanName) {
+//
+//        // 注解注入属性支持@Autowired(spring提供), @Resource(JSR250规范), @Inject(JSR330规范)
+//        // @Autowired和@Inject使用AutowiredAnnotationBeanPostProcessor类实现属性注入
+//        // @Resource使用CommonAnnotationBeanPostProcessor类实现属性注入
+//        // 重写此方法实现属性注入逻辑
+//
+//        return pvs;
+//    }
+
+
+}
