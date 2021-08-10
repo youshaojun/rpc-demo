de48c31cb165e71019c5793688489660a32b3717
yousj <yousj@yczcjk.com>
2021-08-10 15:12:30 +0800

    优化代码

--- rpc/src/main/java/com/example/rpc/registry/config/MyRedisStandaloneConfiguration.java
+++ rpc/src/main/java/com/example/rpc/registry/config/MyRedisStandaloneConfiguration.java
@@ -2,15 +2,18 @@ package com.example.rpc.registry.config;
 
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 
+import java.util.Optional;
+
 /**
  * @author yousj
  * @date 2021/8/7
  */
 public class MyRedisStandaloneConfiguration extends RedisStandaloneConfiguration {
-    public MyRedisStandaloneConfiguration(String hostName,String password,int port, int database){
-        super.setHostName(hostName);
-        super.setPassword(password);
-        super.setPort(port);
-        super.setDatabase(database);
+
+    public MyRedisStandaloneConfiguration(String hostName, String password, Integer port, Integer database) {
+        Optional.ofNullable(hostName).ifPresent(e -> super.setHostName(hostName));
+        Optional.ofNullable(password).ifPresent(e -> super.setPassword(password));
+        Optional.ofNullable(port).ifPresent(e -> super.setPort(port));
+        Optional.ofNullable(database).ifPresent(e -> super.setDatabase(database));
     }
 }
