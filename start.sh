64a9edbf47473520240e50b4fecca91f851956e4
yousj <ryan.you@hupun.com>
2021-08-15 18:30:30 +0800

    调整启动脚本

--- start.sh
+++ start.sh
@@ -5,10 +5,22 @@ do
   e)
   if [[ "${OPTARG}" == "dev" ]]  || [[ "${OPTARG}" == "test" ]] || [[ "${OPTARG}" == "prod" ]];
   then
-      echo "当前环境设置为==> " ${OPTARG}
-      sed -i 's#SPRING_PROFILES_ACTIVE=.*$#SPRING_PROFILES_ACTIVE='${OPTARG}'#g' environment.env
+      read -r -p "你确定将环境激活为[ ${OPTARG} ]吗? [Y/N] " input
+      case ${input} in
+        [yY][eE][sS]|[yY])
+		sed -i 's#SPRING_PROFILES_ACTIVE=.*$#SPRING_PROFILES_ACTIVE='${OPTARG}'#g' environment.env
+		;;
+        [nN][oO]|[nN])
+		echo "退出......"
+		exit 1
+       	;;
+        *)
+		echo "请输入[Y/N]"
+		exit 1
+		;;
+        esac
   else
-      echo "输入环境不合法, -e [dev|test|prod]"
+      echo "请输入[-e dev|test|prod]"
       exit 1
   fi
   ;;
@@ -17,7 +29,7 @@ do
   sed -i 's#APP_VERSION=.*$#APP_VERSION='${OPTARG}'#g' environment.env
   ;;
   ?)
-  echo "未知参数, params[-e|-v]"
+  echo "请输入[-e|-v]"
   exit 1;;
  esac
 done
