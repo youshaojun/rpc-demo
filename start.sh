a7590a59d8b2f336f1bde489f51cc97e118be70b
yousj <yousj@yczcjk.com>
2021-08-20 14:32:40 +0800

    脚本调整

--- start.sh
+++ start.sh
@@ -50,9 +50,9 @@ sed -i 's#SPRING_PROFILES_ACTIVE=.*$#SPRING_PROFILES_ACTIVE='${SPRING_PROFILES_A
 c1=consumer
 c2=producer 
 c3=register-center
-docker stop $c1 $c2 $c3
-docker rm $c1 $c2 $c3
-docker rmi $c1 $c2 $c3
+docker stop ${c1} ${c2} ${c3}
+docker rm ${c1} ${c2} ${c3}
+docker rmi ${c1} ${c2} ${c3}
 docker-compose up -d
 
 
