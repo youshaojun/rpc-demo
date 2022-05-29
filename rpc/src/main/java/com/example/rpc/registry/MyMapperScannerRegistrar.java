58af65358a497e1084af483f265ea62f91c13a10
小明 <youshaojunde@163.com>
2022-05-29 11:18:32 +0000

    update rpc/src/main/java/com/example/rpc/registry/MyMapperScannerRegistrar.java.

--- rpc/src/main/java/com/example/rpc/registry/MyMapperScannerRegistrar.java
+++ rpc/src/main/java/com/example/rpc/registry/MyMapperScannerRegistrar.java
@@ -4,7 +4,11 @@ import com.example.rpc.annotation.MapperScan;
 import com.example.rpc.registry.factorybean.MyMapperFactoryBean;
 import com.example.rpc.registry.mapper.BaseMapper;
 import org.reflections.Reflections;
+import org.springframework.beans.factory.annotation.AnnotatedBeanDefinition;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.BeanDefinitionHolder;
 import org.springframework.beans.factory.support.*;
+import org.springframework.context.annotation.ClassPathBeanDefinitionScanner;
 import org.springframework.context.annotation.ImportBeanDefinitionRegistrar;
 import org.springframework.core.annotation.AnnotationAttributes;
 import org.springframework.core.type.AnnotationMetadata;
@@ -18,28 +22,63 @@ import java.util.Set;
  * @since 2021/8/7
  */
 public class MyMapperScannerRegistrar implements ImportBeanDefinitionRegistrar {
-
     @Override
     public void registerBeanDefinitions(AnnotationMetadata importingClassMetadata, BeanDefinitionRegistry registry) {
         doScan(registry, importingClassMetadata);
     }
-
+    /**
+     * 扫描获取需要整合的类
+     */
     private void doScan(BeanDefinitionRegistry registry, AnnotationMetadata importingClassMetadata) {
         AnnotationAttributes mapperScanAttrs = AnnotationAttributes.fromMap(importingClassMetadata.getAnnotationAttributes(MapperScan.class.getName()));
         if (mapperScanAttrs != null) {
-            Set<Class<? extends BaseMapper>> beanClasses = new Reflections(mapperScanAttrs.get("value")).getSubTypesOf(BaseMapper.class);
-            registerBeanDefinitions(registry, beanClasses);
+            ClassPathMapperScan scanner = new ClassPathMapperScan(registry);
+            scanner.doScan(mapperScanAttrs.getStringArray("value"));
         }
     }
 
-    private void registerBeanDefinitions(BeanDefinitionRegistry registry, Set<Class<? extends BaseMapper>> beanClasses) {
-        for (Class<?> beanClass : beanClasses) {
-            BeanDefinitionBuilder builder = BeanDefinitionBuilder.genericBeanDefinition(beanClass);
-            GenericBeanDefinition definition = (GenericBeanDefinition) builder.getRawBeanDefinition();
-            definition.getConstructorArgumentValues().addGenericArgumentValue(beanClass);
-            definition.setBeanClass(MyMapperFactoryBean.class);
-            definition.setAutowireMode(GenericBeanDefinition.AUTOWIRE_BY_TYPE);
-            registry.registerBeanDefinition(beanClass.getSimpleName(), definition);
+    /**
+     * ClassPathMapperScanner 直接从Mybatis中将关键代码复制过来
+     * 注意这里将源码中的许多代码删掉了
+     */
+    public class ClassPathMapperScan extends ClassPathBeanDefinitionScanner {
+        public ClassPathMapperScan(BeanDefinitionRegistry registry) {
+            super(registry, false);
+        }
+
+        @Override
+        public Set<BeanDefinitionHolder> doScan(String... basePackages) {
+            addIncludeFilter((metadataReader, metadataReaderFactory) -> true);
+            Set<BeanDefinitionHolder> beanDefinitions = super.doScan(basePackages);
+            if (!beanDefinitions.isEmpty()) {
+                processBeanDefinitions(beanDefinitions);
+            }
+            return beanDefinitions;
+        }
+
+        private void processBeanDefinitions(Set<BeanDefinitionHolder> beanDefinitions) {
+            GenericBeanDefinition definition;
+            for (BeanDefinitionHolder holder : beanDefinitions) {
+                definition = (GenericBeanDefinition) holder.getBeanDefinition();
+                String beanClassName = definition.getBeanClassName();
+                definition.getConstructorArgumentValues().addGenericArgumentValue(beanClassName);
+                definition.setBeanClass(MyMapperFactoryBean.class);
+                definition.setAutowireMode(GenericBeanDefinition.AUTOWIRE_BY_TYPE);
+            }
+        }
+
+        @Override
+        protected boolean isCandidateComponent(AnnotatedBeanDefinition beanDefinition) {
+            return beanDefinition.getMetadata().isInterface() && beanDefinition.getMetadata().isIndependent();
+        }
+
+        @Override
+        protected boolean checkCandidate(String beanName, BeanDefinition beanDefinition) {
+            if (super.checkCandidate(beanName, beanDefinition)) {
+                return true;
+            } else {
+                return false;
+            }
         }
     }
 }
